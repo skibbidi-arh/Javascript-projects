edc4fed1954ee032357c2dd70745eded773b4059
Arham Apon <arhamaponutsho@gmail.com>
2024-11-13 12:41:25 +0600

    1

---
+++ Basics JS/index.js
@@ -0,0 +1,20 @@
+
+
+let name = 'lol';
+
+typeof name;
+
+let ok = true;
+typeof ok;
+console.log(typeof ok);
+console.log(typeof name);
+
+let person = {
+name: 'Utsho',
+age : 30
+
+};
+
+console.log(person);
+
+console.log(person.age);
